1dd1e13f8fee7fa16694d229d87ca692536dba37
Chris Bryer <cjbryer@gmail.com>
2011-06-21 00:09:19 -0700

    modified instructions in _config.php

--- _config.php
+++ _config.php
@@ -1,16 +1,22 @@
 <?php
 
 /**
- * Register one locale per top level domain, and one locale per virtualhost
- * Usage:
- * <code>
- * TranslatableDomains::addDomainHandler('com','en_US');
- * TranslatableDomains::addDomainHandler('de','de_DE');
- * TranslatableDomains::addDomainHandler('jp','ja_JP');
+ * Register one locale per top level domain, and one locale per virtualhost.
+ * mixing virtual hosts and live domains in the same array is not recommended as 
+ * a live webserver may try to switch to a domain name of localhost.
  *
- * TranslatableDomains::addLocalhostDomainHandler('localhost-en','en_US');
- * TranslatableDomains::addLocalhostDomainHandler('localhost-fr','fr_FR');
- * TranslatableDomains::addLocalhostDomainHandler('localhost','en_US');
+ * Usage on live web server:
+ * <code>
+ * TranslatableDomains::addDomainHandler('mysite.com','en_US');
+ * TranslatableDomains::addDomainHandler('mysite.de','de_DE');
+ * TranslatableDomains::addDomainHandler('mysite.jp','ja_JP');
+ * </code>
+ *
+ * Usage on localhost
+ * <code>
+ * TranslatableDomains::addDomainHandler('localhost-en:8888','en_US');
+ * TranslatableDomains::addDomainHandler('localhost-fr:8888','fr_FR');
+ * TranslatableDomains::addDomainHandler('localhost:8888','en_US');
  * </code>
  *
  */
